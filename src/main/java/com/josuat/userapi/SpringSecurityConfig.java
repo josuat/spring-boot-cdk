e0b03c08daeda8eace3b326d2ec76140d1a3bb0d
Josua Troesch <josuat@users.noreply.github.com>
2020-11-08 01:34:13 +0800

    Allow unauthenticated access to health endpoint

--- src/main/java/com/josuat/userapi/SpringSecurityConfig.java
+++ src/main/java/com/josuat/userapi/SpringSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.josuat.userapi;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,6 +15,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     http
         .csrf().disable()
         .authorizeRequests()
+        .antMatchers(HttpMethod.GET, "/actuator/health").permitAll()
         .anyRequest().authenticated()
         .and().httpBasic();
   }
